0fd9fa357e8d4ec773ab8585287172d06c540776
RuiJ72 <rlagos900@gmail.com>
2022-09-30 01:24:11 -0300

    First method HELPER implemented

---
+++ UniversityCoursesApi/Helpers/JwtHelpers.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Claims;
+using UniversityCoursesApi.Models.DataModels;
+
+namespace UniversityCoursesApi.Helpers
+{
+    public static class JwtHelpers
+    {
+        public static IEnumerable<Claim> GetClaims(this UserTokens userAccounts, Guid Id)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim("Id", userAccounts.Id.ToString()),
+                new Claim(ClaimTypes.Name, userAccounts.UserName),
+                new Claim(ClaimTypes.Email, userAccounts.EmailId),
+                new Claim(ClaimTypes.NameIdentifier, Id.ToString()),
+                new Claim(ClaimTypes.Expiration, DateTime.UtcNow.AddDays(1).ToString("MMM ddd dd yyyy Hh:mm:ss tt")),
+            };
+
+            if(userAccounts.UserName == "Admin")
+            {
+                claims.Add(new Claim(ClaimTypes.Role, "Administrator"));
+            }else if(userAccounts.UserName == "User 1")
+            {
+                claims.Add(new Claim(ClaimTypes.Role, "User"));
+                claims.Add(new Claim("UserOnly", "User 1"));
+            }
+
+            return claims;
+        }
+
+    }
+}
+
---
+++ UniversityCoursesApi/Models/DataModels/JtwSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace UniversityCoursesApi.Models.DataModels
+{
+    public class JtwSettings
+    {
+        public bool ValidateIssuerSigningKey { get; set; }
+        public string? IssuerSigningKey { get; set; }
+        
+        public bool ValidateIssuer { get; set; } = true;
+        public string? ValidIssuer { get; set; }
+        
+        public bool ValidateAudience { get; set; } = true;
+        public string? ValidAudience { get; set; }
+
+        public bool RequireExpirationTime { get; set; }
+        public bool ValidateLifetime { get; set; } = true;
+
+    }
+}
